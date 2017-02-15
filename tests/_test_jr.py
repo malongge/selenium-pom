1de13cd613671638041adce8727ccb56e7220a91
malongge <ma_longge@sina.com>
2017-02-15 19:43:27 +0800

    移除信息

--- tests/_test_jr.py
+++
@@ -1,8 +0,0 @@
-from pages.jr_page import JRPage
-
-
-class TestJRPage(object):
-    def test_zero_buy(self, base_url, selenium, existing_user):
-        jr_pg = JRPage(base_url, selenium)
-        jr_pg.get_relative_path('/showhome/index?f=1')
-        jr_pg.click_zero_buy()
